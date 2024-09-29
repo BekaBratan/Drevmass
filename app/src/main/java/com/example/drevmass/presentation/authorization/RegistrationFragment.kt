3e4fce010bae61fc1ac147a81e711138849aedac
BekaBratan <210103206@stu.sdu.edu.kz>
2024-09-29 12:54:41 +0500

    Add a clear button on authorization pages and initialize formating function for phone number in registration page. Add a listeners for edit texts

--- app/src/main/java/com/example/drevmass/presentation/authorization/RegistrationFragment.kt
+++ app/src/main/java/com/example/drevmass/presentation/authorization/RegistrationFragment.kt
@@ -3,18 +3,24 @@ package com.example.drevmass.presentation.authorization
 import android.annotation.SuppressLint
 import android.graphics.Rect
 import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.text.Editable
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
 import com.example.drevmass.R
 import com.example.drevmass.databinding.FragmentRegistrationBinding
 
+@Suppress("DEPRECATION")
 class RegistrationFragment : Fragment() {
 
     private lateinit var binding: FragmentRegistrationBinding
@@ -111,7 +117,134 @@ class RegistrationFragment : Fragment() {
                 }
                 false
             }
+
+            binding.run {
+                etEmail.addTextChangedListener {
+                    if (etEmail.text.toString().isNotEmpty()){
+                        setDrawableEnd(etEmail)
+                    }
+                }
+
+                etEmail.setOnFocusChangeListener { _, event ->
+                    if (event) {
+                        normalEditText(etEmail)
+                        if (etEmail.text.toString().isNotEmpty()){
+                            setDrawableEnd(etEmail)
+                        }
+                    } else {
+                        removeDrawableEnd(etEmail)
+                        if (!isValidEmail(etEmail.text.toString()) && etEmail.text.toString().isNotEmpty()) {
+                            wrongEditText(etEmail)
+                        }
+                    }
+                }
+
+                etEmail.setOnTouchListener { _, event ->
+                    if (event.action == MotionEvent.ACTION_UP) {
+                        val drawableEnd = binding.etPassword.compoundDrawables[2]
+                        if (event.rawX >= (binding.etPassword.right - drawableEnd.bounds.width())) {
+                            clearText(etEmail)
+                            return@setOnTouchListener true
+                        }
+                    }
+                    false
+                }
+
+                etName.addTextChangedListener {
+                    if (etName.text.toString().isNotEmpty()){
+                        setDrawableEnd(etName)
+                    }
+                }
+
+                etName.setOnFocusChangeListener { _, event ->
+                    if (event) {
+                        if (etName.text.toString().isNotEmpty()){
+                            setDrawableEnd(etName)
+                        }
+                    } else {
+                        removeDrawableEnd(etName)
+                    }
+                }
+
+                etName.setOnTouchListener { _, event ->
+                    if (event.action == MotionEvent.ACTION_UP) {
+                        val drawableEnd = binding.etPassword.compoundDrawables[2]
+                        if (event.rawX >= (binding.etPassword.right - drawableEnd.bounds.width())) {
+                            clearText(etName)
+                            return@setOnTouchListener true
+                        }
+                    }
+                    false
+                }
+
+                etPhone.addTextChangedListener {
+                    if (etPhone.text.toString().isNotEmpty()){
+                        setDrawableEnd(etPhone)
+                    }
+                }
+
+                etPhone.setOnFocusChangeListener { _, event ->
+                    if (event) {
+                        if (etPhone.text.toString().isNotEmpty()){
+                            setDrawableEnd(etPhone)
+                        }
+                    } else {
+                        removeDrawableEnd(etPhone)
+                    }
+                }
+
+                etPhone.setOnTouchListener { _, event ->
+                    if (event.action == MotionEvent.ACTION_UP) {
+                        val drawableEnd = binding.etPassword.compoundDrawables[2]
+                        if (event.rawX >= (binding.etPassword.right - drawableEnd.bounds.width())) {
+                            clearText(etPhone)
+                            return@setOnTouchListener true
+                        }
+                    }
+                    false
+                }
+            }
+
+            binding.etPhone.addTextChangedListener(object : PhoneNumberFormattingTextWatcher() {
+                private var isUpdating = false
+                private val phoneNumberPattern = "+# ### ### ####"
+
+                override fun afterTextChanged(s: Editable?) {
+                    if (isUpdating) {
+                        isUpdating = false
+                        return
+                    }
+
+                    val unformatted = s.toString().replace("\\D".toRegex(), "")
+                    val formatted = formatPhoneNumber(unformatted)
+
+                    isUpdating = true
+                    s?.replace(0, s.length, formatted)
+                }
+
+                private fun formatPhoneNumber(number: String): String {
+                    var formatted = ""
+                    var i = 0
+                    var prev = ""
+
+                    phoneNumberPattern.forEach { char ->
+                        if (char == '#' && i < number.length) {
+                            if (prev.isNotEmpty()) {
+                                formatted += prev
+                                prev = ""
+                            }
+                            formatted += number[i]
+                            i++
+                        } else if (char != '#') {
+                            prev = char.toString()
+                        }
+                    }
+
+                    return formatted
+                }
+            })
         }
+
     }
 
     private fun isAllFilled(): Boolean {
@@ -134,5 +267,38 @@ class RegistrationFragment : Fragment() {
         // Move the cursor to the end of the text
         binding.etPassword.setSelection(binding.etPassword.text.length)
         isPasswordVisible = !isPasswordVisible
+    }
+
+    private fun clearText(editText: EditText) {
+        editText.text.clear()
+        val drawableStart = editText.compoundDrawables[0]
+        editText.setCompoundDrawablesWithIntrinsicBounds(drawableStart, null, null, null)
+    }
+
+    private fun setDrawableEnd(editText: EditText) {
+        val drawableStart = editText.compoundDrawables[0]
+        val drawableEnd = ContextCompat.getDrawable(requireContext(), R.drawable.ic_clear)
+        editText.setCompoundDrawablesWithIntrinsicBounds(drawableStart, null, drawableEnd, null)
+    }
+
+    private fun removeDrawableEnd(editText: EditText) {
+        val drawableStart = editText.compoundDrawables[0]
+        editText.setCompoundDrawablesWithIntrinsicBounds(drawableStart, null, null, null)
+    }
+
+    @SuppressLint("UseCompatLoadingForColorStateLists", "UseCompatTextViewDrawableApis")
+    private fun wrongEditText(editText: EditText) {
+        editText.compoundDrawableTintList = resources.getColorStateList(R.color.coral_1000)
+        editText.backgroundTintList = resources.getColorStateList(R.color.coral_1000)
+    }
+
+    @SuppressLint("UseCompatTextViewDrawableApis", "UseCompatLoadingForColorStateLists")
+    private fun normalEditText(editText: EditText) {
+        editText.compoundDrawableTintList = null
+        editText.backgroundTintList = resources.getColorStateList(R.color.gray_500)
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
